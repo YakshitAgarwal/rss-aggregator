6a05b8c1f93694b31af0f7a8f773cee8a6f32015
Yakshit Agarwal <yakshitagarwal05@gmail.com>
2026-07-01 21:56:16 +0530

    user table completed, working on feed table

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/YakshitAgarwal/rss-aggregator/internal/database"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
-func main(){
+type apiConfig struct {
+	DB *database.Queries
+}
+
+func main() {
 	godotenv.Load()
 
 	portString := os.Getenv("PORT")
@@ -18,43 +26,59 @@ func main(){
 		log.Fatal("PORT not found in environment")
 	}
 
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL not found in environment")
+	}
+
+	connection, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to database:", err)
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(connection),
+	}
+
 	router := chi.NewRouter()
 
 	router.Use(cors.Handler(cors.Options{
 		//specifies which websites (origins) are allowed to access the API
-		AllowedOrigins: []string{"https://*", "http://*"}, 
+		AllowedOrigins: []string{"https://*", "http://*"},
 
 		//the HTTP methods browsers are allowed to use when making cross-origin requests
-		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}, 
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 
 		//any request header is allowed
-		AllowedHeaders: []string{"*"}, 
+		AllowedHeaders: []string{"*"},
 
 		ExposedHeaders: []string{"Link"},
-		
-		//do not allow browsers to send credentials with cross-origin requests, 
+
+		//do not allow browsers to send credentials with cross-origin requests,
 		//should be false in case of REST APIs using JWT
 		//should be true in case of web applications using cookies
-		AllowCredentials: false, 
+		AllowCredentials: false,
 
 		//the browser remembers the preflight result for 300 seconds
-		MaxAge: 300, 
+		MaxAge: 300,
 	}))
 
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
+	v1Router.Get("/users", apiCfg.handlerGetUser)
 
 	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
 		Handler: router,
-		Addr: ":" + portString,
+		Addr:    ":" + portString,
 	}
 
 	log.Printf("Server starting on port %v", portString)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
